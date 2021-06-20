2de0dea2216acdc933fc1244bd6e62d43765e1c2
ning1_wang@163.com <ning1_wang@163.com>
2021-06-21 06:49:35 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue'
+import router from './router'
+// 引入封装请求
+import {postRequest} from './utils/api'
+import {getRequest} from './utils/api'
+import {deleteRequest} from './utils/api'
+import {putRequest} from './utils/api'
+
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+// 封装成vue插件 使用请求
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
